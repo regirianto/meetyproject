2819101fc39a461367a00ccfbb47e253b95d199e
parukazz <parukazziyi@gmail.com>
2025-01-14 15:43:44 +0700

    add color & font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-primary": "#267F53",
+        "accent-blue": "#99B7F5",
+      },
+      fontFamily: {
+        main: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
