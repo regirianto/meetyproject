46bc85b6fa33704fbe4ba27aafba7082b8be9490
parukazz <parukazziyi@gmail.com>
2025-01-14 15:44:12 +0700

    add welcome page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,15 @@ import AuthLayout from "./_auth/AuthLayout";
 import SignInForm from "./_auth/form/SignInForm";
 import SignUpForm from "./_auth/form/SignUpForm";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import { Home, Welcome } from "./_root/pages";
 
 const App = () => {
   return (
     <main>
       <Routes>
+        {/* Welcome page */}
+        <Route path="/welcome" element={<Welcome />} />
+
         {/* Public Routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SignInForm />} />
