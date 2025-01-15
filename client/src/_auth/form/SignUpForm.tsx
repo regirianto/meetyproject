ade977d77c7e0bd6b5cb0d3a8aefe3a6ec617c83
parukazz <parukazziyi@gmail.com>
2025-01-15 11:35:09 +0700

    styling signupform (not yet)

--- client/src/_auth/form/SignUpForm.tsx
+++ client/src/_auth/form/SignUpForm.tsx
@@ -1,9 +1,18 @@
-
-
 const SignUpForm = () => {
   return (
-    <div>SignUpForm</div>
-  )
-}
+    <main>
+      <div className="text-green-primary ">
+        <h2 className="font-semibold text-2xl">Welcome!</h2>
+        <p className="text-sm mt-2">
+          Let's get started by creating your account. Please fill in the details
+          below to join us.
+        </p>
+      </div>
+      <div>
+        <form></form>
+      </div>
+    </main>
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
