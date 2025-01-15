7bdb5ddfdb920eeabd16ce2ee796f217443a9c95
parukazz <parukazziyi@gmail.com>
2025-01-15 11:21:21 +0700

    make button reuseble

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,14 +1,21 @@
 interface ButtonProps {
   label: string;
+  bgColor?: string;
+  isLink: boolean;
+  href?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
-  return (
-    <>
-      <button className="bg-white font-bold text-xs text-black w-full py-[10px] rounded-[7px]">
-        {label}
-      </button>
-    </>
+const Button: React.FC<ButtonProps> = ({ label, bgColor, isLink, href }) => {
+  const classes = `${
+    bgColor == "primary text-white" ? "bg-green-primary" : "bg-white text-black"
+  } font-bold text-xs  w-full py-[10px] rounded-[7px]`;
+
+  return isLink ? (
+    <a href={href} className={classes}>
+      {label}
+    </a>
+  ) : (
+    <button className={classes}>{label}</button>
   );
 };
 
