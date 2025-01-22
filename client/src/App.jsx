827401e7526be5ea3fd0a3749921467386f10bd6
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-22 22:35:31 +0700

    make Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./_root/pages/Welcome";
 import RootLayout from "./_root/RootLayout";
 import Home from "./_root/pages/Home";
+import AuthLayout from "./_auth/AuthLayout";
+import SignUpForm from "./_auth/form/SignUpForm";
+import SignInForm from "./_auth/form/SignInForm";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/" element={<RootLayout />}>
+        <Route path="/" element={<Welcome />} />
+
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="/sign-up" element={<SignUpForm />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
           <Route path="/home" element={<Home />} />
         </Route>
       </Routes>
