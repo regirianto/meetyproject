b5c80e8b7b784f5927daf04d5cb2f9fee0a89c34
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-22 22:35:16 +0700

    add icons

---
+++ client/public/img/icons/index.js
@@ -0,0 +1,3 @@
+export { default as facebookIcon } from "./facebook-icon.svg";
+export { default as googleIcon } from "./google-icon.svg";
+export { default as linkedinIcon } from "./linkedin-icon.svg";
