878f387ab322c174aea884b431d5640f2bfa1cfd
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-22 22:36:36 +0700

    fixed button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,28 @@
+/* eslint-disable react/prop-types */
+const Button = ({ label, type, isLink, href }) => {
+  const classes = `${
+    type == "primary"
+      ? "bg-primary text-white"
+      : type == "secondary"
+      ? "bg-white text-black"
+      : type == "outline"
+      ? "bg-transparent text-white border-[1px] border-white"
+      : ""
+  }  w-full h-[52px] flex justify-center items-center text-base font-semibold rounded-md`;
 
+  if (isLink === true) {
+    return (
+      <a href={href} className={classes}>
+        {label}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} type="submit">
+        {label}
+      </button>
+    );
+  }
+};
 
-const Button = () => {
-  return (
-    <button className="bg-white w-full h-[52px] text-base font-semibold rounded-md">
-      Create account
-    </button>
-  )
-}
-
-export default Button
+export default Button;
