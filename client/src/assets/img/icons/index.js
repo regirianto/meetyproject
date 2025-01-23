56b325f1d01fae58b102ca8739530fbac1e8bfc5
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-23 19:44:36 +0700

    add new icons

--- client/src/assets/img/icons/index.js
+++ client/src/assets/img/icons/index.js
@@ -1,3 +1,5 @@
 export { default as facebookIcon } from "./facebook-icon.svg";
 export { default as googleIcon } from "./google-icon.svg";
 export { default as linkedinIcon } from "./linkedin-icon.svg";
+export { default as maleIcon } from "./male-icon.svg";
+export { default as femaleIcon } from "./female-icon.svg";
