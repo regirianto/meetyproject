fd500287a500e20b1c218f8919de59e439ee4333
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-23 19:45:41 +0700

    initial commit

---
+++ client/src/components/Dropdown.jsx
@@ -0,0 +1,14 @@
+const Dropdown = () => {
+  return (
+    <div>
+      <label htmlFor="day" className="text-sm">
+        Day
+      </label>
+      <button>
+        
+      </button>
+    </div>
+  );
+};
+
+export default Dropdown;
