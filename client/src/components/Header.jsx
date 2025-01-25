d33baff7d3e38b6ac5c0ee66c30a1adecf872bb3
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-26 01:17:27 +0700

    add new props (icon)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
-const Header = ({ heading, desc }) => {
+const Header = ({ heading, desc, icon }) => {
   return (
     <div className="flex flex-col gap-1 text-primary">
-      <h2 className="text-2xl font-semibold">{heading}</h2>
-      <p className="text-sm font-light leading-6">
-        {desc}
-      </p>
+      <div className="flex items-center gap-2">
+        <h2 className="text-2xl font-semibold">{heading}</h2>
+        <img src={icon} className="-mt-2" />
+      </div>
+      <p className="text-sm font-light leading-6">{desc}</p>
     </div>
   );
 };
