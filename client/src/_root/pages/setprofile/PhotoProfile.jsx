89d653807d03438b04fe9be9f8981a2f17d57b0c
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-26 01:17:01 +0700

    create SetPhotoProfile page

--- client/src/_root/pages/setprofile/PhotoProfile.jsx
+++ client/src/_root/pages/setprofile/PhotoProfile.jsx
@@ -1,9 +1,19 @@
-
+import Header from "../../../components/Header";
+import { cameraIcon } from "../../../assets/img/icons";
 
 const PhotoProfile = () => {
   return (
-    <div>PhotoProfile</div>
-  )
-}
+    <main className="template-parent-box">
+      <Header
+        heading="Show yourself off!"
+        icon={cameraIcon}
+        desc="Upload a photo to make your profile stand out and attract the right connections."
+      />
 
-export default PhotoProfile
+      {/* Photo box */}
+      <div></div>
+    </main>
+  );
+};
+
+export default PhotoProfile;
