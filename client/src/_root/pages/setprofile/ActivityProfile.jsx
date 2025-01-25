102f75d27f1f250753f959672756180772242419
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-26 01:16:31 +0700

    create ActivityProfile page

--- client/src/_root/pages/setprofile/ActivityProfile.jsx
+++ client/src/_root/pages/setprofile/ActivityProfile.jsx
@@ -1,8 +1,33 @@
+import Button from "../../../components/Button";
+import Header from "../../../components/Header";
 
 const ActivityProfile = () => {
   return (
-    <div>ActivityProfile</div>
-  )
-}
+    <main className="template-parent-box">
+      <Header
+        heading="Represent your vibe!"
+        desc="Pick an activity that speaks to your true self. What's your perfect match?"
+      />
 
-export default ActivityProfile
+      {/* form */}
+      <div>
+        <form className="form-base-profile">
+          <label htmlFor="interest">
+            Choose the one that feels most like you!
+          </label>
+
+          <div>
+            <Button
+              label="Next"
+              type="primary"
+              isLink={true}
+              href="/set-photo"
+            />
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default ActivityProfile;
