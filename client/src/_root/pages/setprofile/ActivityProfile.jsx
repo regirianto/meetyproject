ecc74d92b786ca7970f3d6951f7097c8d28198d9
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-27 14:12:30 +0700

    create list interest ui & function when checked

--- client/src/_root/pages/setprofile/ActivityProfile.jsx
+++ client/src/_root/pages/setprofile/ActivityProfile.jsx
@@ -1,22 +1,85 @@
+import { useEffect, useState } from "react";
 import Button from "../../../components/Button";
 import Header from "../../../components/Header";
+import { PlusIcon } from "../../../assets/img/icons";
+import { interestList } from "../../../constant";
 
 const ActivityProfile = () => {
+  const [isChecked, setIsChecked] = useState([]);
+  const [scrollActive, setScrollActive] = useState(false);
+
+  const handleCheckbox = (id, event) => {
+    if (event.target.checked) {
+      console.log("checked ", id);
+      setIsChecked((prev) => [...prev, id]);
+    } else {
+      console.log("unchecked ", id);
+      setIsChecked((prev) => prev.filter((checkedId) => checkedId !== id));
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const targetHeight = 500;
+
+      scrollPosition > targetHeight
+        ? setScrollActive(true)
+        : setScrollActive(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <main className="template-parent-box">
-      <Header
-        heading="Represent your vibe!"
-        desc="Pick an activity that speaks to your true self. What's your perfect match?"
-      />
+      <div className="flex flex-col gap-6 fixed right-6 left-6 top-20">
+        <Header
+          heading="Represent your vibe!"
+          desc="Pick an activity that speaks to your true self. What's your perfect match?"
+        />
+
+        <p className="font-medium text-sm">
+          Choose the one that feels most like you!
+        </p>
+      </div>
 
       {/* form */}
-      <div>
+      <div className="mt-36">
         <form className="form-base-profile">
-          <label htmlFor="interest">
-            Choose the one that feels most like you!
-          </label>
+          <div className="flex flex-wrap gap-[10px]">
+            {/* interest list */}
+            {interestList.map((interest) => (
+              <div key={interest.id}>
+                <label
+                  htmlFor={`check-${interest.id}`}
+                  className={`${
+                    isChecked.includes(interest.id)
+                      ? "bg-primary text-white"
+                      : "bg-tertiary text-primary"
+                  }  font-normal w-max flex gap-1 py-2 pl-[16px] pr-3 rounded-full border-[1px] border-primary`}
+                >
+                  {interest.label}
+                  <PlusIcon
+                    color={`${
+                      isChecked.includes(interest.id) ? "#FFFFFF" : "#267F53"
+                    }`}
+                  />
+                </label>
+                <input
+                  type="checkbox"
+                  checked={isChecked.includes(interest.id)}
+                  onChange={(e) => handleCheckbox(interest.id, e)}
+                  className="hidden"
+                  id={`check-${interest.id}`}
+                />
+              </div>
+            ))}
+          </div>
 
-          <div>
+          <div className="button-box-next">
             <Button
               label="Next"
               type="primary"
