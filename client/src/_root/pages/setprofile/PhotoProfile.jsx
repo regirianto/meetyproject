9bfa6416121b1602bc321ae3e88f4bb666332a64
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-29 11:46:01 +0700

    create upload photo function

--- client/src/_root/pages/setprofile/PhotoProfile.jsx
+++ client/src/_root/pages/setprofile/PhotoProfile.jsx
@@ -1,7 +1,33 @@
 import Header from "../../../components/Header";
-import { cameraIcon } from "../../../assets/img/icons";
+import { cameraIcon, PlusIcon } from "../../../assets/img/icons";
+import { useRef, useState } from "react";
+import Button from "../../../components/Button";
 
 const PhotoProfile = () => {
+  const numInputFile = 6;
+  const fileInputRef = useRef([]);
+  const [files, setFiles] = useState(Array(numInputFile).fill(null));
+
+  const handleButtonPhoto = (index) => {
+    if (fileInputRef.current[index]) {
+      fileInputRef.current[index].click();
+    }
+  };
+
+  const handleChange = (index, e) => {
+    if (e.target.files.length > 0) {
+      const selectedFile = e.target.files[0];
+      console.log(`Selected file for input ${index}`, selectedFile);
+
+      // Update the file state
+      setFiles((prevFiles) => {
+        const newFiles = [...prevFiles];
+        newFiles[index] = URL.createObjectURL(selectedFile);
+        return newFiles;
+      });
+    }
+  };
+
   return (
     <main className="template-parent-box">
       <Header
@@ -11,7 +37,48 @@ const PhotoProfile = () => {
       />
 
       {/* Photo box */}
-      <div></div>
+      <div>
+        <form>
+          <div className="grid grid-flow-row grid-cols-3 gap-[10px] w-full">
+            {Array.from({ length: numInputFile }, (_, index) => (
+              <div
+                key={index}
+                className="bg-tertiary border-2 border-primary border-dashed rounded-md overflow-hidden aspect-square flex justify-center items-center"
+              >
+                <button onClick={() => handleButtonPhoto(index)} type="button">
+                  {/* Show PlusIcon when no file is selected */}
+                  {!files[index] && <PlusIcon color="#267F53" size={48} />}
+                </button>
+                {/* Show image preview when a file is selected */}
+                {files[index] && (
+                  <img
+                    src={files[index]}
+                    alt={`Preview ${index}`}
+                    className="w-full h-full object-cover"
+                  />
+                )}
+
+                <input
+                  type="file"
+                  ref={(e) => (fileInputRef.current[index] = e)}
+                  onChange={(e) => handleChange(index, e)}
+                  accept="image/*"
+                  className="hidden"
+                />
+              </div>
+            ))}
+          </div>
+
+          <div className="button-box-next">
+            <Button
+              label="Next"
+              type="primary"
+              isLink={true}
+              href="/home"
+            />
+          </div>
+        </form>
+      </div>
     </main>
   );
 };
