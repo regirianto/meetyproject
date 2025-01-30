b07f467ec08b2a74099ad22dcf2a7e6e9ec567e9
Paruk Azziyi <parukazziyi@gmail.com>
2025-01-30 22:21:19 +0700

    design root template

--- client/src/_root/RootLayout.jsx
+++ client/src/_root/RootLayout.jsx
@@ -1,12 +1,42 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import logo from "../assets/img/logo-horizontal.svg";
+import { menuList } from "../constant";
 
 const RootLayout = () => {
   return (
-    <main>
-      <Outlet />
-    </main>
-  )
-}
+    <main className="app-container">
+      {/* Heading */}
+      <div className="flex justify-between items-center">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="w-36">
+          <h4 className="font-secondary w-full uppercase text-right text-primary font-bold">
+            find what you&apos;re searching for
+          </h4>
+        </div>
+      </div>
 
-export default RootLayout
+      {/* Content */}
+      <Outlet />
+
+      {/* Menu bar */}
+      <div>
+        <ul className="flex justify-between items-center">
+          {menuList.map((item) => (
+            <li key={item.id}>
+              <button
+                className="flex flex-col gap-2 items-center"
+                type="button"
+              >
+                <span className="text-sm">{item.label}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
