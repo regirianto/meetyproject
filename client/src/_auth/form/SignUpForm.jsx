7d1534dc33a105aafe4a3337cc6e83222434f7f8
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-03 14:39:50 +0700

    create sign-up function

--- client/src/_auth/form/SignUpForm.jsx
+++ client/src/_auth/form/SignUpForm.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
+import { signUp } from "../../api";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signUp(form);
+      alert(res.data.message);
+      navigate("/sign-in");
+    } catch (error) {
+      console.error("API ERROR: ", error.response?.data || error);
+      alert(error.response?.data?.error || "Something ain't right");
+    }
+  };
+
   return (
     <main className="flex flex-col gap-8">
-      <Header heading="Welcome!" desc="Let's get started by creating your account.." />
+      <Header
+        heading="Welcome!"
+        desc="Let's get started by creating your account.."
+      />
 
       {/* form */}
       <div>
-        <form className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <div className="flex flex-col gap-2">
             <label htmlFor="username" className="font-medium text-sm">
               Username
@@ -16,7 +46,9 @@ const SignUpForm = () => {
             <input
               type="text"
               placeholder="Enter your username"
+              name="username"
               className="input-class"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -26,7 +58,9 @@ const SignUpForm = () => {
             <input
               type="email"
               placeholder="Enter your@email.com"
+              name="email"
               className="input-class"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -36,7 +70,9 @@ const SignUpForm = () => {
             <input
               type="number"
               placeholder="62xxxxxxxxxx"
+              name="phone"
               className="input-class"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -46,14 +82,16 @@ const SignUpForm = () => {
             <input
               type="password"
               placeholder="Enter your password"
+              name="password"
               className="input-class"
+              onChange={handleChange}
             />
           </div>
           <div className="mt-3">
             <Button
               label="Create account"
               type="primary"
-              isLink={true}
+              isLink={false}
               href="/base-profile"
             />
           </div>
