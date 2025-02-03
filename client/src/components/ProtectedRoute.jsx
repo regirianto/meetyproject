84381adb07fbfce1902a65a8346f08aa128d2b31
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-03 14:40:39 +0700

    create component for protect the route

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  return token ? children : <Navigate to="/sign-in" />;
+};
+
+export default ProtectedRoute;
