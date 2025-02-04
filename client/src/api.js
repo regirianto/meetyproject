71c1ad3d35072e307f965584c0d5efd50db59bcc
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-04 13:45:18 +0700

    add api getInterests

--- client/src/api.js
+++ client/src/api.js
@@ -9,6 +9,7 @@ export const signUp = (data) => API.post("/auth/sign-up", data);
 export const signIn = (data) => API.post("/auth/sign-in", data);
 export const saveBaseProfile = (data) =>
   API.post("/profile/base-profile", data);
+export const getInterests = () => API.get("/profile/interests");
 export const saveInterestProfile = (data) =>
   API.post("/profile/activity", data);
 export const uploadPhotoProfile = (data) =>
