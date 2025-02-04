5eba54e64baad185b242b18d786ab5dd3c9d18c6
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-04 13:45:31 +0700

    create function for interest

--- client/src/_root/pages/setprofile/ActivityProfile.jsx
+++ client/src/_root/pages/setprofile/ActivityProfile.jsx
@@ -1,19 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../../../components/Button";
 import Header from "../../../components/Header";
 import { PlusIcon } from "../../../assets/img/icons";
-import { interestList } from "../../../constant";
+import { useNavigate } from "react-router-dom";
+import { getInterests, saveInterestProfile } from "../../../api";
 
 const ActivityProfile = () => {
-  const [isChecked, setIsChecked] = useState([]);
+  const [interests, setInterests] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const navigate = useNavigate();
+  const profileId = JSON.parse(localStorage.getItem("profileId"));
 
-  const handleCheckbox = (id, event) => {
-    if (event.target.checked) {
-      console.log("checked ", id);
-      setIsChecked((prev) => [...prev, id]);
+  // Fetch all interest from the server
+  useEffect(() => {
+    const fetchInterests = async () => {
+      try {
+        const res = await getInterests();
+        setInterests(res.data);
+      } catch (error) {
+        console.error("Error fetching interests", error);
+      }
+    };
+    fetchInterests();
+  }, []);
+
+  // Handle Checkbox
+  const handleCheckbox = (event) => {
+    const interestId = parseInt(event.target.value);
+    if (selected.includes(interestId)) {
+      setSelected(selected.filter((id) => id !== interestId));
     } else {
-      console.log("unchecked ", id);
-      setIsChecked((prev) => prev.filter((checkedId) => checkedId !== id));
+      setSelected([...selected, interestId]);
+    }
+  };
+
+  // Handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (selected.length === 0) {
+      alert("Please select at least one interest.");
+      return;
+    }
+
+    console.log("📤 Sending Data:", { profileId, interestId: selected });
+
+    try {
+      await saveInterestProfile({ profileId, interestId: selected });
+      alert("Interests saved successfully!");
+      navigate("/set-photo");
+    } catch (error) {
+      console.error("❌ API Error:", error.response?.data || error);
     }
   };
 
@@ -32,44 +68,40 @@ const ActivityProfile = () => {
 
       {/* form */}
       <div className="mt-36">
-        <form className="form-base-profile">
+        <form onSubmit={handleSubmit} className="form-base-profile">
           <div className="flex flex-wrap gap-[10px]">
             {/* interest list */}
-            {interestList.map((interest) => (
-              <div key={interest.id}>
-                <label
-                  htmlFor={`check-${interest.id}`}
-                  className={`${
-                    isChecked.includes(interest.id)
-                      ? "bg-primary text-white"
-                      : "bg-tertiary text-primary"
-                  }  font-normal w-max flex gap-1 py-2 pl-[16px] pr-3 rounded-full border-[1px] border-primary`}
-                >
-                  {interest.label}
-                  <PlusIcon
-                    color={`${
-                      isChecked.includes(interest.id) ? "#FFFFFF" : "#267F53"
-                    }`}
+
+            {interests.map((interest) => {
+              const isSelected = selected.includes(interest.id);
+
+              return (
+                <div key={interest.id}>
+                  <label
+                    htmlFor={`check-${interest.id}`}
+                    className={`cursor-pointer ${
+                      isSelected
+                        ? "bg-primary text-tertiary"
+                        : "bg-tertiary text-primary"
+                    }  font-normal w-max flex gap-1 py-2 pl-[16px] pr-3 rounded-full border-[1px] border-primary`}
+                  >
+                    {interest.interest_name}
+                    <PlusIcon color={`${isSelected ? "#FFFFFF" : "#267F53"}`} />
+                  </label>
+                  <input
+                    type="checkbox"
+                    value={interest.id}
+                    onChange={handleCheckbox}
+                    className="hidden"
+                    id={`check-${interest.id}`}
                   />
-                </label>
-                <input
-                  type="checkbox"
-                  checked={isChecked.includes(interest.id)}
-                  onChange={(e) => handleCheckbox(interest.id, e)}
-                  className="hidden"
-                  id={`check-${interest.id}`}
-                />
-              </div>
-            ))}
+                </div>
+              );
+            })}
           </div>
 
           <div className="button-box-next">
-            <Button
-              label="Next"
-              type="primary"
-              isLink={true}
-              href="/set-photo"
-            />
+            <Button label="Next" type="primary" isLink={false} />
           </div>
         </form>
       </div>
