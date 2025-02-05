7e25dae7cec72945327ef48561ab3bf03c186293
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-05 21:32:09 +0700

    add flexibel heading text and scrollable content

--- client/src/_root/RootLayout.jsx
+++ client/src/_root/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import logo from "../assets/img/logo-horizontal.svg";
-import { menuList } from "../constant";
+import { menuList, pageHeadings } from "../constant";
 
 const RootLayout = () => {
   // Log out function
@@ -17,8 +17,11 @@ const RootLayout = () => {
   // Menu bar active
   const location = useLocation();
 
+  // Heading Text
+  const headingText = pageHeadings[location.pathname] || "Welcome!";
+
   return (
-    <main className="app-container">
+    <main className="app-container flex flex-col h-screen">
       {/* Heading */}
       <div className="flex justify-between items-center z-50">
         <div>
@@ -35,13 +38,15 @@ const RootLayout = () => {
 
         <div className="w-36">
           <h4 className="font-secondary w-full uppercase text-right text-primary font-bold">
-            find what you&apos;re searching for
+            {headingText}
           </h4>
         </div>
       </div>
 
       {/* Content */}
-      <Outlet />
+      <div className="flex-1 overflow-y-auto w-full h-full pb-20">
+        <Outlet />
+      </div>
 
       {/* Menu bar */}
       <div className="fixed left-0 right-0 bottom-0 px-2 py-4 border-t-2 bg-white">
