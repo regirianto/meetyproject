29dc936392e8f0b80a8412e9878b2bfcb0c8436c
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-05 20:03:31 +0700

    create menu bar active

--- client/src/_root/RootLayout.jsx
+++ client/src/_root/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import logo from "../assets/img/logo-horizontal.svg";
 import { menuList } from "../constant";
 
@@ -9,8 +9,13 @@ const RootLayout = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
+    localStorage.removeItem("profileId");
+    localStorage.clear();
     navigate("/sign-in");
   };
+
+  // Menu bar active
+  const location = useLocation();
 
   return (
     <main className="app-container">
@@ -41,17 +46,30 @@ const RootLayout = () => {
       {/* Menu bar */}
       <div className="fixed left-0 right-0 bottom-0 px-2 py-4 border-t-2 bg-white">
         <ul className="flex justify-between items-center gap-1">
-          {menuList.map((item) => (
-            <li key={item.id} className="w-1/5 flex justify-center">
-              <button
-                className="flex flex-col items-center h-14 justify-between"
-                type="button"
-              >
-                <item.icon size={28} />
-                <span className="text-sm">{item.label}</span>
-              </button>
-            </li>
-          ))}
+          {menuList.map((item) => {
+            const isActive = location.pathname.includes(item.path);
+            return (
+              <li key={item.id} className="w-1/5 flex justify-center">
+                <button
+                  className="flex flex-col items-center h-14 justify-between"
+                  type="button"
+                  onClick={() => navigate(`/${item.path}`)}
+                >
+                  <item.icon
+                    size={28}
+                    color={`${isActive ? "#267F53" : "#0F172A"}`}
+                  />
+                  <span
+                    className={`text-sm ${
+                      isActive ? "text-primary" : "text-black"
+                    }`}
+                  >
+                    {item.label}
+                  </span>
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </main>
