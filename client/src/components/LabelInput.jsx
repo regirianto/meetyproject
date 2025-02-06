8f900cb6bc6d62f3c14db0779aec4567b3b44b56
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-06 14:18:19 +0700

    modify labelInput

--- client/src/components/LabelInput.jsx
+++ client/src/components/LabelInput.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/prop-types */
-const LabelInput = ({ labelFor, label, addClasses, icon }) => {
-  const classes = `text-[13px] ${addClasses} flex gap-2 items-center`;
+const LabelInput = ({
+  labelFor,
+  label,
+  addClasses,
+  icon,
+  textSize = "text-[13px]",
+}) => {
+  const classes = `${textSize} ${addClasses} flex gap-2 items-center tracking-tight`;
 
   return (
     <label htmlFor={labelFor} className={classes}>
