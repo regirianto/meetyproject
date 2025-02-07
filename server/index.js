fb7da7dc833d08440e00c0f89227f6a7dea106b8
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-07 11:13:35 +0700

    like server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profile.js";
+import likesRoutes from "./routes/likes.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/uploads", express.static("uploads"));
+app.use("/api/likes", likesRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
