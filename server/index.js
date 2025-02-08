4731ec66c78fdbf3dfe7e38646832da6b9ef6649
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-08 12:05:23 +0700

    rizki

--- server/index.js
+++ server/index.js
@@ -11,24 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const allowedOrigins = [
-  "meety-frontend-beta.vercel.app",
-  "http://localhost:5173",
-];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET, POST, PUT, DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.options("*", cors());
 
@@ -41,3 +25,4 @@ app.use("/api/chat", chatRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
