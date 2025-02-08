4c31d77a1d8fb948ece4934d7844ac3316adfac9
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-08 13:11:08 +0700

    fixxxx

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://meety-server.vercel.app/api",
+  baseURL: "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
