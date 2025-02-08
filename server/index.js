7b75a0fe139c9b41a5acd2524263f47c5d6f654e
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-08 12:22:56 +0700

    deploy-2

--- server/index.js
+++ server/index.js
@@ -38,6 +38,3 @@ app.use("/api/profile", profileRoutes);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/likes", likesRoutes);
 app.use("/api/chat", chatRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
